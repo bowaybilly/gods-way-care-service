fb693f4100aab081062134a2ad3934fff11f1b51
anthony bobbie <anthony.bobbie@hotmail.com>
2022-11-09 18:25:21 +0000

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css',
+    '../assets/assets/bootstrap/css/bootstrap-grid.min.css',
+    '../assets/assets/bootstrap/css/bootstrap-reboot.min.css',
+    '../assets/assets/bootstrap/css/bootstrap.min.css',
+    '../assets/assets/dropdown/css/style.css',
+    '../assets/assets/mobirise/css/mbr-additional.css',
+    '../assets/assets/web/assets/mobirise-icons2/mobirise2.css',
+    '../assets/assets/theme/css/style.css',
+    '../assets/assets/socicon/css/styles.css',
+    
+  ]
+})
+export class AppComponent {
+  title = 'gods-way-care-service';
+}
